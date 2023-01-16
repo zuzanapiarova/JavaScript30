7f5e5b632f5d155f8a64322e173d9b0860def3a6
Zuzana Piarová <zuzana.piarova1@gmail.com>
2023-01-16 14:47:21 +0100

    finished JS

---
+++ 01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,19 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); 
+    if(!audio) return; //stop the function from executin when there is no audio for that key, bs keys with no audio will show null
+    key.classList.add('playing');
+    audio.currentTime = 0;  // always rewinds the audio to start when hitting the key 
+    audio.play();
+}
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return; //skip if it not a transform css property
+    this.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+console.log(keys);
+
+window.addEventListener('keydown', playSound); // no () after calling the function in event listeners!!
